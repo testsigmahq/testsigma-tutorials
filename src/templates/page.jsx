5b3cc485fe5a974f144ffa8ccce51cb71cc98e8d
PratheepV <pratheep.velicherla@gmail.com>
2022-01-24 20:29:29 +0530

    Footer changes

--- src/templates/page.jsx
+++ src/templates/page.jsx
@@ -11,7 +11,6 @@ import SEO from "../components/seo";
 import {Link} from "../../.cache/gatsby-browser-entry";
 import SearchInputBox from "../components/SearchInputBox";
 import MobileView from "../components/MobileView";
-import SubNav from "../components/SubNav";
 import Footer from "../components/Footer"
 
 export default ({ data, pageContext }) => {
@@ -111,40 +110,41 @@ export default ({ data, pageContext }) => {
                             </aside>
                         </div>
                         <div className="pagination_buttons">
+                            <div className={prev ? 'flex justify-between' : 'overflow-hidden'}>
+                                {prev && (
+                                    <div className="footer_prev">
+                                    <Link to={prev.url}>
+                                        <span>Previous</span>
+                                        <svg stroke="" fill="#78757a" stroke-width="0"
+                                             viewBox="0 0 24 24" className="css-1hyj6ne" height="1.6em" width="1.6em"
+                                             xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
+                                        </svg>
+                                        <h3 className="perviousLink">
+                                            {prev.title}</h3>
 
-                            {prev && (
-                                <div className="footer_prev">
-                                <Link to={prev.url}>
-                                    <span>Previous</span>
-                                    <svg stroke="" fill="#78757a" stroke-width="0"
-                                         viewBox="0 0 24 24" className="css-1hyj6ne" height="1.6em" width="1.6em"
-                                         xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
-                                    </svg>
-                                    <h3 className="perviousLink">
-                                        {prev.title}</h3>
+                                    </Link>
+                                    </div>
+                                )}
+                                {next && (
+                                    <div className="next_button">
+                                    <Link to={next.url}>
+                                        <span>Next</span>
+                                        <svg stroke="" fill="#78757a" stroke-width="0" viewBox="0 0 24 24"
+                                             className="css-jmo9lw" height="1.6em" width="1.6em"
+                                             xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path>
+                                        </svg>
+                                        <h3 className="next_link">
+                                            {next.title}
+                                        </h3>
+                                    </Link>
 
-                                </Link>
-                                </div>
-                            )}
-                            {next && (
-                                <div className="next_button">
-                                <Link to={next.url}>
-                                    <span>Next</span>
-                                    <svg stroke="" fill="#78757a" stroke-width="0" viewBox="0 0 24 24"
-                                         className="css-jmo9lw" height="1.6em" width="1.6em"
-                                         xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path>
-                                    </svg>
-                                    <h3 className="next_link">
-                                        {next.title}
-                                    </h3>
-                                </Link>
+                                    </div>
 
-                                </div>
-
-                            )}
+                                )}
+                            </div>
                             <Footer/>
                         </div>
 
