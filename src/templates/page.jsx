b79d1afb9a4ef1b2fbc16f2fa5e415e3496b7cb5
Rahul Mohan <111055521+RahulMohan7@users.noreply.github.com>
2022-11-22 16:28:45 +0530

    [3.3.2][TE-6165]: Use same Freshchat session in app and website (#19)
    * [3.3.2][TE-6165]: Use same Freshchat session in app and website
    
    * [3.3.2][TE-6165]: Update package-lock.json

--- src/templates/page.jsx
+++ src/templates/page.jsx
@@ -84,7 +84,7 @@ export default ({ data, pageContext }) => {
                         </nav>
                         <div className="relative flex items-center justify-between w-2/5 pl-9 pr-9">
                             <SearchInputBox></SearchInputBox>
-                            <a className="border border-green-600 font-bold px-4 py-1.5 text-green-600 rounded" href='https://testsigma.com/signup'>Get started for free</a>
+                            <a id={'signup-btn'} target={'_blank'} className="border border-green-600 font-bold px-4 py-1.5 text-green-600 rounded" href='https://testsigma.com/signup'>Get started for free</a>
                         </div>
                     </nav>
                 </div>
